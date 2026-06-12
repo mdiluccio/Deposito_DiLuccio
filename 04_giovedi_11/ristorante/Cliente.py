d87586f465afe4f12188cf4f78274d6e9e1e921d
mdiluccio <m.diluccio92@gmail.com>
2026-06-12 12:53:42 +0200

    commit finale Ristorante esame

--- 04_giovedi_11/ristorante/Cliente.py
+++ 04_giovedi_11/ristorante/Cliente.py
@@ -2,31 +2,58 @@ import Articolo
 
 class Cliente:
     
-    def __init__(self, nome):
+    def __init__(self, nome, username, password):
+        self.username = username
+        self.password = password
         self.nome = nome
         self.carrello = [] #lista di articoli
         
+        
+    def is_logged(self, username, password):
+        if self.username == username and self.password == password:
+            print("Benvenuto", self.nome)
+            
+            return True
+        else:
+            print("Username o password errati")
+            return False
+        
+    #def registrati(self, cliente):
+     #   cliente = Cliente()
+        
+        
     def visualizza_articoli(self):
         for articolo in self.carrello:
-            print("Cilente", self.nome, "- Articolo", articolo.nome, "Prezzo", articolo.prezzo, "Quantità", articolo.quantita)
+            print(self.nome, ":\n- Articolo:", articolo.nome, "Prezzo:", articolo.prezzo, "Quantità:", articolo.quantita)
             
+        if self.carrello == []:
+            print(self.nome, ":\n nessun articolo")    
           
-    def acquista(self, articolo, quantita_richiesta):
-        if articolo.quantita >= quantita_richiesta:
+    def acquista(self, nuovo_articolo, quantita_richiesta):
+        if nuovo_articolo.quantita >= quantita_richiesta:
              
-            articolo_da_comprare = Articolo.Articolo(articolo.nome, articolo.prezzo, quantita_richiesta)
-            self.carrello.append(articolo_da_comprare)
+            nuovo_articolo_acquisto = Articolo.Articolo(nuovo_articolo.nome, nuovo_articolo.prezzo, quantita_richiesta)
+            self.carrello.append(nuovo_articolo_acquisto)
             
-            #decremento la quantita
-            articolo.quantita -= quantita_richiesta 
-            
-            
+            #decremento la quantita dell'invnetario
+            nuovo_articolo.quantita -= quantita_richiesta 
+    
+    
+    def tracking(func):
+        def wrapper(*args, **kwargs):
+            print("Sto tenendo traccia degli acquisti")
+            risultato = func(*args, **kwargs)
+            return risultato
+        return wrapper 
+    
+                
+    @tracking        
     def traccia_acquisti(self):
         for articolo_in_carrello in self.carrello: 
-            print("Il cliente", self.nome, "ha acquistato", articolo_in_carrello.quantita)  
+            print("Il cliente", self.nome, "ha acquistato", articolo_in_carrello.quantita, "articolo/i")  
         
         if self.carrello == []:
             print("Il cliente", self.nome, "non ha effettuato nessun acquisto")
     
             
-            
+           
--- 04_giovedi_11/ristorante/Inventario.py
+++ 04_giovedi_11/ristorante/Inventario.py
@@ -9,9 +9,27 @@ class Inventario:
         for articolo in self.articoli:
               print("Inventario - Articolo", articolo.nome, "Prezzo", articolo.prezzo, "Quantità", articolo.quantita)
               
-              
+    def rimuovi_articoli(self, articoli_selezionati):
+        for articolo_selezionato in articoli_selezionati:
+            self.articoli.remove(articolo_selezionato)
+            print("Articolo", articolo_selezionato.nome, "rimosso")
+            
+            
+            
+    def aggiorna_articolo(self, articolo_modificato):
+        for articolo_selezionato in self.inventario:
+            if articolo_selezionato.nome == articolo_modificato.nome:
+                articolo_selezionato.nome = articolo_modificato.nome
+            if articolo_selezionato.prezzo == articolo_modificato.prezzo:
+                articolo_selezionato.prezzo = articolo_modificato.prezzo
+                
+            self.aricoli.replace(articolo_selezionato)
+            print("Articolo", articolo_selezionato.nome, "modificato")
+            
+            
     def aggiungi_articoli(self, articoli):
         for articolo in articoli:
-            self.articoli.append(articolo) 
+            self.articoli.append(articolo)           
+    
             
 
